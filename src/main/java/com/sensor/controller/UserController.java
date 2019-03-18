41839a657b2f615c757a9952f2b32810c4199fb2
1406423298@qq.com <1406423298@qq.com>
2019-03-18 18:05:07 +0800

    finish

--- src/main/java/com/sensor/controller/UserController.java
+++ src/main/java/com/sensor/controller/UserController.java
@@ -37,8 +37,8 @@ public class UserController {
 
     @ApiOperation(value = "用户登录", notes = "根据用户和密码进行登录")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", value = "用户名", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "password", value = "用户密码", required = true, dataType = "String")
+            @ApiImplicitParam(name = "username", value = "用户名", required = true, paramType = "form", dataType = "String"),
+            @ApiImplicitParam(name = "password", value = "用户密码", required = true, paramType = "form", dataType = "String")
     })
     @PostMapping("/login")
     public ResponseResult<User> login(@RequestParam("username") String username, @RequestParam("password") String password) {
@@ -53,7 +53,7 @@ public class UserController {
      */
 
     @ApiOperation(value = "检查用户名是否存在", notes = "检查用户名是否存在")
-    @ApiImplicitParam(name = "username", value = "用户名", required = true, dataType = "String")
+    @ApiImplicitParam(name = "username", value = "用户名", required = true, paramType = "form", dataType = "String")
     @GetMapping("/checkUser")
     public ResponseResult<Boolean> checkUser(@RequestParam("username") String username) {
         return ResponseResult.e(ResponseCode.OK, userService.checkUserName(username));
@@ -67,9 +67,9 @@ public class UserController {
      */
     @ApiOperation(value = "用户注册", notes = "根据用户和密码和身份证进行注册")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "username", value = "用户名", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "password", value = "用户密码", required = true, dataType = "String"),
-            @ApiImplicitParam(name = "card", value = "身份证号", required = true, dataType = "String")
+            @ApiImplicitParam(name = "username", value = "用户名", required = true, paramType = "form", dataType = "String"),
+            @ApiImplicitParam(name = "password", value = "用户密码", required = true, paramType = "form", dataType = "String"),
+            @ApiImplicitParam(name = "card", value = "身份证号", required = true, paramType = "form", dataType = "String")
     })
     @PostMapping("/save")
     public ResponseResult<Boolean> save(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("card") String card) {
